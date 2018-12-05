1794e4954cce6c8118d7b65af8a47f379570e17e
Lance Hilliard <lance@digitalcreations.cc>
2018-12-05 01:33:11 -0600

    [2018] Day 5 both stars

--- AdventOfCode.2018/Program.Day5.cs
+++ AdventOfCode.2018/Program.Day5.cs
@@ -2,40 +2,46 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 
 namespace AdventOfCode._2018 {
     static partial class Program {
         static void Day5() {
             var input = File.ReadLines("inputs/Day5Input.txt").First();
+            var simpleCollapsedPolymer = React(input);
+            Report($"Part 1: {simpleCollapsedPolymer.Length}");
 
-            var startingLength = input.Length;
-            var startingIndex = input.Length-1;
-            var stable = false;
-            while (!stable) {
-                var chars = input.ToCharArray();
-                int i;
-                for (i = startingIndex; i > 0; i--) {
-                    var thisChar = chars[i];
-                    var previousCharacter = chars[i-1];
-                    var reacts = string.Equals($"{thisChar}".ToLower(), $"{previousCharacter}".ToLower(), StringComparison.InvariantCultureIgnoreCase) && thisChar!=previousCharacter;
-                    if (reacts) {
-                        //Console.WriteLine("reacts!");
-                        input = input.Remove(i - 1, 2);
-                        startingIndex = i-1;
+            var collapsedPolymerLengths = new Dictionary<string, string>();
+            for (var character = 'A'; character <= 'Z'; character++) {
+                var letter = $"{character}";
+                var unitAdjustedPolymer = input.Replace(letter, String.Empty, StringComparison.InvariantCultureIgnoreCase);
+                var adjustedCollapsedPolymer = React(unitAdjustedPolymer);
+                collapsedPolymerLengths[letter] = adjustedCollapsedPolymer;
+            }
+            Report($"Part 2: {collapsedPolymerLengths.OrderBy(x => x.Value.Length).First().Value.Length.ToString()}");
+
+            string React(string polymer) {
+                var startingIndex = polymer.Length - 1;
+                while (true) {
+                    var chars = polymer.ToCharArray();
+                    int polymerIndex;
+                    for (polymerIndex = startingIndex; polymerIndex > 0; polymerIndex--) {
+                        var unit1Char = chars[polymerIndex];
+                        var unit2Char = chars[polymerIndex - 1];
+                        var lowerCaseUnit1Letter = $"{unit1Char}".ToLower();
+                        var lowerCaseUnit2Letter = $"{unit2Char}".ToLower();
+                        var reacts = string.Equals(lowerCaseUnit1Letter, lowerCaseUnit2Letter, StringComparison.InvariantCultureIgnoreCase) && unit1Char != unit2Char;
+                        if (reacts) {
+                            startingIndex = polymerIndex - (polymerIndex.Equals(polymer.Length-1) ? 2 : 1);
+                            polymer = polymer.Remove(polymerIndex - 1, 2);
+                            break;
+                        }
+                    }
+                    if (polymerIndex == 0) {
                         break;
                     }
                 }
-
-                //Console.WriteLine(i);
-                if (i == 0) {
-                    stable = true;
-                }
+                return polymer;
             }
-
-            Report(input.Length.ToString());
-
-
         }
     }
 }
