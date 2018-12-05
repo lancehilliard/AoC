49c7abe447e9defb1ae91a1417f80e1a30395327
Lance Hilliard <lance@digitalcreations.cc>
2018-12-05 01:55:11 -0600

    refactor

--- AdventOfCode.2018/Program.Day5.cs
+++ AdventOfCode.2018/Program.Day5.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode._2018 {
     static partial class Program {
         static void Day5() {
-            var input = File.ReadLines("inputs/Day5Input.txt").First();
-            var simpleCollapsedPolymer = React(input);
-            Report($"Part 1: {simpleCollapsedPolymer.Length}");
+            var input = File.ReadAllText("inputs/Day5Input.txt");
+            Report($"Part 1: {React(input).Length}");
 
             var collapsedPolymerLengths = new Dictionary<string, string>();
             for (var character = 'A'; character <= 'Z'; character++) {
                 var letter = $"{character}";
-                var unitAdjustedPolymer = input.Replace(letter, String.Empty, StringComparison.InvariantCultureIgnoreCase);
+                var unitAdjustedPolymer = input.Replace(letter, string.Empty, StringComparison.InvariantCultureIgnoreCase);
                 var adjustedCollapsedPolymer = React(unitAdjustedPolymer);
                 collapsedPolymerLengths[letter] = adjustedCollapsedPolymer;
             }
@@ -21,23 +21,15 @@ namespace AdventOfCode._2018 {
 
             string React(string polymer) {
                 var startingIndex = polymer.Length - 1;
-                while (true) {
+                var polymerIndex = startingIndex;
+                while (polymerIndex > 0) {
                     var chars = polymer.ToCharArray();
-                    int polymerIndex;
                     for (polymerIndex = startingIndex; polymerIndex > 0; polymerIndex--) {
-                        var unit1Char = chars[polymerIndex];
-                        var unit2Char = chars[polymerIndex - 1];
-                        var lowerCaseUnit1Letter = $"{unit1Char}".ToLower();
-                        var lowerCaseUnit2Letter = $"{unit2Char}".ToLower();
-                        var reacts = string.Equals(lowerCaseUnit1Letter, lowerCaseUnit2Letter, StringComparison.InvariantCultureIgnoreCase) && unit1Char != unit2Char;
-                        if (reacts) {
+                        if (Math.Abs(chars[polymerIndex]-chars[polymerIndex - 1])==32) {
                             startingIndex = polymerIndex - (polymerIndex.Equals(polymer.Length-1) ? 2 : 1);
                             polymer = polymer.Remove(polymerIndex - 1, 2);
                             break;
                         }
-                    }
-                    if (polymerIndex == 0) {
-                        break;
                     }
                 }
                 return polymer;
