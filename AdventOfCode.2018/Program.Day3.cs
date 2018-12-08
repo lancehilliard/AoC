cb79e3f68ed6e03f7b076ec8004843a73c291af2
Lance Hilliard <lance@digitalcreations.cc>
2018-12-07 22:00:48 -0600

    [2018] Day 3 remove dependency class

--- AdventOfCode.2018/Program.Day3.cs
+++ AdventOfCode.2018/Program.Day3.cs
@@ -8,21 +8,21 @@ namespace AdventOfCode._2018 {
         static void Day3() {
             var inputs = File.ReadLines("inputs/Day3Input.txt");
             var grid = new (int id, List<int> ids)[1000, 1000];
-            var fabricClaims = new List<FabricClaim>();
+            var fabricClaims = new List<(int id, int x, int y, int width, int height)>();
             foreach (var input in inputs) {
                 var atSignParts = input.Split("@");
                 var colonParts = atSignParts[1].Split(":");
                 var commaParts = colonParts[0].Split(",");
                 var xParts = colonParts[1].Split("x");
                 var id = Convert.ToInt32(atSignParts[0].Replace("#",string.Empty).Trim());
-                var firstColumn = Convert.ToInt32(commaParts[0].Trim())+1;
-                var firstRow = Convert.ToInt32(commaParts[1].Trim())+1;
+                var x = Convert.ToInt32(commaParts[0].Trim())+1;
+                var y = Convert.ToInt32(commaParts[1].Trim())+1;
                 var width = Convert.ToInt32(xParts[0].Trim());
                 var height = Convert.ToInt32(xParts[1].Trim());
-                var fabricClaim = new FabricClaim(id, firstColumn, firstRow, width, height);
+                var fabricClaim = (id, x, y, width, height);
                 fabricClaims.Add(fabricClaim);
-                for (var i = firstColumn; i <= firstColumn-1+width; i++) {
-                    for (var j = firstRow; j <= firstRow-1+height; j++) {
+                for (var i = x; i <= x-1+width; i++) {
+                    for (var j = y; j <= y-1+height; j++) {
                         if (grid[i - 1, j - 1].ids == null) {
                             grid[i - 1, j - 1].ids = new List<int>();
                         }
@@ -48,8 +48,8 @@ namespace AdventOfCode._2018 {
             foreach (var fabricClaim in fabricClaims) {
                 var totalLocations = 0;
                 var locationsWithoutOverlap = 0;
-                for (var i = fabricClaim.FirstColumn-1; i < fabricClaim.LastColumn; i++) {
-                    for (var j = fabricClaim.FirstRow - 1; j < fabricClaim.LastRow; j++) {
+                for (var i = fabricClaim.x-1; i < fabricClaim.x+fabricClaim.width-1; i++) {
+                    for (var j = fabricClaim.y - 1; j < fabricClaim.y+fabricClaim.height-1; j++) {
                         totalLocations++;
                         if (grid[i, j].ids != null && grid[i, j].ids.Count == 1) {
                             locationsWithoutOverlap++;
@@ -57,7 +57,7 @@ namespace AdventOfCode._2018 {
                     }
                 }
                 if (totalLocations == locationsWithoutOverlap) {
-                    Report($"Part 2: {fabricClaim.Id}");
+                    Report($"Part 2: {fabricClaim.id}");
                     break;
                 }
             }
