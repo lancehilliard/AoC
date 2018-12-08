cc7a76619f34a4134448a7ad8a758322eefe1bb0
Lance Hilliard <lance@digitalcreations.cc>
2018-12-07 22:36:40 -0600

    [2018] Day 3 refactor

--- AdventOfCode.2018/Program.Day3.cs
+++ AdventOfCode.2018/Program.Day3.cs
@@ -7,57 +7,45 @@ namespace AdventOfCode._2018 {
     static partial class Program {
         static void Day3() {
             var inputs = File.ReadLines("inputs/Day3Input.txt");
-            var grid = new (int id, List<int> ids)[1000, 1000];
-            var fabricClaims = new List<(int id, int x, int y, int width, int height)>();
-            foreach (var input in inputs) {
-                var atSignParts = input.Split("@");
-                var colonParts = atSignParts[1].Split(":");
-                var commaParts = colonParts[0].Split(",");
-                var xParts = colonParts[1].Split("x");
-                var id = Convert.ToInt32(atSignParts[0].Replace("#",string.Empty).Trim());
-                var x = Convert.ToInt32(commaParts[0].Trim())+1;
-                var y = Convert.ToInt32(commaParts[1].Trim())+1;
-                var width = Convert.ToInt32(xParts[0].Trim());
-                var height = Convert.ToInt32(xParts[1].Trim());
-                var fabricClaim = (id, x, y, width, height);
-                fabricClaims.Add(fabricClaim);
-                for (var i = x; i <= x-1+width; i++) {
-                    for (var j = y; j <= y-1+height; j++) {
-                        if (grid[i - 1, j - 1].ids == null) {
-                            grid[i - 1, j - 1].ids = new List<int>();
-                        }
-                        grid[i - 1, j - 1].ids.Add(id);
-                    }
-                }
-            }
-
+            var grid = new List<int>[1000, 1000];
+            var claims = new List<(int id, int x, int y, int width, int height)>();
             var overlappingInches = 0;
-            for (var i = 0; i < 1000; i++) {
-                for (var j = 0; j < 1000; j++) {
-                    var location = grid[i,j];
-                    if (location.ids != null) {
-                        var idsCount = location.ids.Count;
-                        if (idsCount > 1) {
+            foreach (var input in inputs) {
+                var atSignParts = input.Split("@").Select(s=>s.Trim()).ToArray();
+                var colonParts = atSignParts[1].Split(":");
+                var commaParts = colonParts[0].Split(",").Select(s=>s.Trim()).ToArray();
+                var xParts = colonParts[1].Split("x").Select(s=>s.Trim()).ToArray();
+                var id = Convert.ToInt32(atSignParts[0].Substring(1));
+                var x = Convert.ToInt32(commaParts[0]);
+                var y = Convert.ToInt32(commaParts[1]);
+                var width = Convert.ToInt32(xParts[0]);
+                var height = Convert.ToInt32(xParts[1]);
+                var claim = (id, x, y, width, height);
+                claims.Add(claim);
+                for (var i = x; i < x+width; i++) {
+                    for (var j = y; j < y+height; j++) {
+                        if (grid[i, j] == null) {
+                            grid[i, j] = new List<int>();
+                        } else if (grid[i, j].Count == 1) {
                             overlappingInches=overlappingInches+1;
                         }
+                        grid[i, j].Add(id);
                     }
                 }
             }
             Report($"Part 1: {overlappingInches}");
 
-            foreach (var fabricClaim in fabricClaims) {
-                var totalLocations = 0;
+            foreach (var (id, x, y, width, height) in claims) {
                 var locationsWithoutOverlap = 0;
-                for (var i = fabricClaim.x-1; i < fabricClaim.x+fabricClaim.width-1; i++) {
-                    for (var j = fabricClaim.y - 1; j < fabricClaim.y+fabricClaim.height-1; j++) {
-                        totalLocations++;
-                        if (grid[i, j].ids != null && grid[i, j].ids.Count == 1) {
+                for (var i = x; i < x+width; i++) {
+                    for (var j = y; j < y+height; j++) {
+                        if (grid[i, j] != null && grid[i, j].Count == 1) {
                             locationsWithoutOverlap++;
                         }
                     }
                 }
-                if (totalLocations == locationsWithoutOverlap) {
-                    Report($"Part 2: {fabricClaim.id}");
+                if (width*height == locationsWithoutOverlap) {
+                    Report($"Part 2: {id}");
                     break;
                 }
             }
